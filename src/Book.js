5304d0caced40936852725e6747e5f07a7479cd1
root <root@ip-172-31-29-99.ec2.internal>
2021-01-17 22:57:43 +0000

    my firt commit

---
+++ src/Book.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+
+function Book(props) {
+    return (
+        <div className="book">
+        <div className="book-top">
+            <div className="book-cover" style={{ width: 128, height: 193, 
+                            backgroundImage: `url("${props.book.imageLinks ? props.book.imageLinks.thumbnail : ''}")`}}>
+            </div>
+            <div className="book-shelf-changer">
+                <select onChange={(event) => props.changeBookShelf(props.book, event.target.value)}
+                value={props.shelfId}>
+                    <option value="move" disabled>Move to...</option>
+                    <option value="currentlyReading">Currently Reading</option>
+                    <option value="wantToRead">Want to Read</option>
+                    <option value="read">Read</option>
+                    <option value="none">None</option>
+                </select>
+            </div>
+        </div>
+        <div className="book-title">{props.book.title}</div>
+        <div className="book-authors">{props.book.authors}</div>
+    </div>
+    )
+}
+
+export default Book
